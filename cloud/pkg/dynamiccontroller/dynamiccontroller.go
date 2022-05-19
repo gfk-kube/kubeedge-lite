vincentgoat/kubeedge

filter resource in dynamiccontroller
Signed-off-by: vincentgoat <linguohui1@huawei.com>

cloud/pkg/dynamiccontroller/dynamiccontroller.go
@@ -17,6 +17,7 @@ limitations under the License.
 package dynamiccontroller
 
 import (
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/dynamic/dynamicinformer"
 	"k8s.io/klog/v2"
 
@@ -27,6 +28,8 @@ import (
 	"github.com/kubeedge/kubeedge/cloud/pkg/common/modules"
 	"github.com/kubeedge/kubeedge/cloud/pkg/dynamiccontroller/application"
 	"github.com/kubeedge/kubeedge/cloud/pkg/dynamiccontroller/config"
+	"github.com/kubeedge/kubeedge/cloud/pkg/dynamiccontroller/filter/defaultmaster"
+	"github.com/kubeedge/kubeedge/cloud/pkg/dynamiccontroller/filter/endpointslice"
 	configv1alpha1 "github.com/kubeedge/kubeedge/pkg/apis/componentconfig/cloudcore/v1alpha1"
 )
 
@@ -38,11 +41,15 @@ type DynamicController struct {
 	applicationCenter            *application.Center
 }
 
-var _ core.Module = (*DynamicController)(nil)
+var (
+	_                 core.Module = (*DynamicController)(nil)
+	dynamicController *DynamicController
+)
 
 func Register(dc *configv1alpha1.DynamicController) {
 	config.InitConfigure(dc)
-	core.Register(newDynamicController(dc.Enable))
+	dynamicController = newDynamicController(dc.Enable)
+	core.Register(dynamicController)
 }
 
 // Name of controller
@@ -62,6 +69,8 @@ func (dctl *DynamicController) Enable() bool {
 
 // Start controller
 func (dctl *DynamicController) Start() {
+	endpointslice.Register()
+	defaultmaster.Register()
 	dctl.dynamicSharedInformerFactory.Start(beehiveContext.Done())
 	for gvr, cacheSync := range dctl.dynamicSharedInformerFactory.WaitForCacheSync(beehiveContext.Done()) {
 		if !cacheSync {
@@ -79,7 +88,8 @@ func newDynamicController(enable bool) *DynamicController {
 		dynamicSharedInformerFactory: informers.GetInformersManager().GetDynamicSharedInformerFactory(),
 	}
 	dctl.applicationCenter = application.NewApplicationCenter(dctl.dynamicSharedInformerFactory)
-
+	dctl.applicationCenter.ForResource(v1.SchemeGroupVersion.WithResource("nodes"))
+	dctl.applicationCenter.ForResource(v1.SchemeGroupVersion.WithResource("services"))
 	return dctl
 }
 

cloud/pkg/dynamiccontroller/filter/util.go
@@ -1,0 +1,48 @@
+package filter
+
+import (
+	"strings"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/informers"
+	"k8s.io/klog/v2"
+
+	commoninformers "github.com/kubeedge/kubeedge/cloud/pkg/common/informers"
+	"github.com/kubeedge/kubeedge/cloud/pkg/groupingcontroller/nodegroup"
+)
+
+func IsBelongToSameGroup(targetNodeName string, epNodeName string) bool {
+	if strings.Compare(targetNodeName, epNodeName) == 0 {
+		return true
+	}
+	targetNode, err := GetDynamicResourceInformer(v1.SchemeGroupVersion.WithResource("nodes")).Lister().Get(targetNodeName)
+	if err != nil {
+		klog.Errorf("node informer get node %s error: %v", targetNodeName, err)
+		return false
+	}
+	targetAccessor, err := meta.Accessor(targetNode)
+	if err != nil {
+		klog.Error(err)
+		return false
+	}
+
+	epNode, err := GetDynamicResourceInformer(v1.SchemeGroupVersion.WithResource("nodes")).Lister().Get(epNodeName)
+	if err != nil {
+		klog.Errorf("node informer get endpoint slice belonging node %s error: %v", epNodeName, err)
+		return false
+	}
+	epNodeAccessor, err := meta.Accessor(epNode)
+	if err != nil {
+		klog.Error(err)
+		return false
+	}
+
+	return targetAccessor.GetLabels()[nodegroup.LabelBelongingTo] == epNodeAccessor.GetLabels()[nodegroup.LabelBelongingTo]
+}
+
+func GetDynamicResourceInformer(gvr schema.GroupVersionResource) informers.GenericInformer {
+	return commoninformers.GetInformersManager().GetDynamicSharedInformerFactory().ForResource(gvr)
+}
+

gfk-kube/kubeedge-lite

Former-commit-id: 47778936e99c5879ee77e508cf2195838c8fa005

cloud/pkg/dynamiccontroller/dynamiccontroller.go
@@ -17,6 +17,7 @@ limitations under the License.
 package dynamiccontroller
 
 import (
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/dynamic/dynamicinformer"
 	"k8s.io/klog/v2"
 
@@ -27,6 +28,8 @@ import (
 	"github.com/kubeedge/kubeedge/cloud/pkg/common/modules"
 	"github.com/kubeedge/kubeedge/cloud/pkg/dynamiccontroller/application"
 	"github.com/kubeedge/kubeedge/cloud/pkg/dynamiccontroller/config"
+	"github.com/kubeedge/kubeedge/cloud/pkg/dynamiccontroller/filter/defaultmaster"
+	"github.com/kubeedge/kubeedge/cloud/pkg/dynamiccontroller/filter/endpointresource"
 	configv1alpha1 "github.com/kubeedge/kubeedge/pkg/apis/componentconfig/cloudcore/v1alpha1"
 )
 
@@ -38,11 +41,15 @@ type DynamicController struct {
 	applicationCenter            *application.Center
 }
 
-var _ core.Module = (*DynamicController)(nil)
+var (
+	_                 core.Module = (*DynamicController)(nil)
+	dynamicController *DynamicController
+)
 
 func Register(dc *configv1alpha1.DynamicController) {
 	config.InitConfigure(dc)
-	core.Register(newDynamicController(dc.Enable))
+	dynamicController = newDynamicController(dc.Enable)
+	core.Register(dynamicController)
 }
 
 // Name of controller
@@ -62,6 +69,8 @@ func (dctl *DynamicController) Enable() bool {
 
 // Start controller
 func (dctl *DynamicController) Start() {
+	endpointresource.Register()
+	defaultmaster.Register()
 	dctl.dynamicSharedInformerFactory.Start(beehiveContext.Done())
 	for gvr, cacheSync := range dctl.dynamicSharedInformerFactory.WaitForCacheSync(beehiveContext.Done()) {
 		if !cacheSync {
@@ -79,7 +88,8 @@ func newDynamicController(enable bool) *DynamicController {
 		dynamicSharedInformerFactory: informers.GetInformersManager().GetDynamicSharedInformerFactory(),
 	}
 	dctl.applicationCenter = application.NewApplicationCenter(dctl.dynamicSharedInformerFactory)
-
+	dctl.applicationCenter.ForResource(v1.SchemeGroupVersion.WithResource("nodes"))
+	dctl.applicationCenter.ForResource(v1.SchemeGroupVersion.WithResource("services"))
 	return dctl
 }
 

cloud/pkg/dynamiccontroller/filter/util.go
@@ -1,0 +1,47 @@
+package filter
+
+import (
+	"strings"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/informers"
+	"k8s.io/klog/v2"
+
+	commoninformers "github.com/kubeedge/kubeedge/cloud/pkg/common/informers"
+	"github.com/kubeedge/kubeedge/cloud/pkg/controllermanager/nodegroup"
+)
+
+func IsBelongToSameGroup(targetNodeName string, epNodeName string) bool {
+	if strings.Compare(targetNodeName, epNodeName) == 0 {
+		return true
+	}
+	targetNode, err := GetDynamicResourceInformer(v1.SchemeGroupVersion.WithResource("nodes")).Lister().Get(targetNodeName)
+	if err != nil {
+		klog.Errorf("node informer get node %s error: %v", targetNodeName, err)
+		return false
+	}
+	targetAccessor, err := meta.Accessor(targetNode)
+	if err != nil {
+		klog.Error(err)
+		return false
+	}
+
+	epNode, err := GetDynamicResourceInformer(v1.SchemeGroupVersion.WithResource("nodes")).Lister().Get(epNodeName)
+	if err != nil {
+		klog.Errorf("node informer get endpoint slice belonging node %s error: %v", epNodeName, err)
+		return false
+	}
+	epNodeAccessor, err := meta.Accessor(epNode)
+	if err != nil {
+		klog.Error(err)
+		return false
+	}
+
+	return targetAccessor.GetLabels()[nodegroup.LabelBelongingTo] == epNodeAccessor.GetLabels()[nodegroup.LabelBelongingTo]
+}
+
+func GetDynamicResourceInformer(gvr schema.GroupVersionResource) informers.GenericInformer {
+	return commoninformers.GetInformersManager().GetDynamicSharedInformerFactory().ForResource(gvr)
+}