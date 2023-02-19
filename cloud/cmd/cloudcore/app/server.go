zhu733756/kubeedge

optimize the iptables manager
Signed-off-by: zhu733756 <zhu733756@kubesphere.io>

cloud/cmd/cloudcore/app/server.go
@@ -23,13 +23,13 @@ import (
 	"github.com/kubeedge/kubeedge/cloud/pkg/cloudhub"
 	"github.com/kubeedge/kubeedge/cloud/pkg/cloudhub/servers/httpserver"
 	"github.com/kubeedge/kubeedge/cloud/pkg/cloudstream"
-	"github.com/kubeedge/kubeedge/cloud/pkg/cloudstream/iptables"
 	"github.com/kubeedge/kubeedge/cloud/pkg/common/client"
 	"github.com/kubeedge/kubeedge/cloud/pkg/common/informers"
 	"github.com/kubeedge/kubeedge/cloud/pkg/common/modules"
 	"github.com/kubeedge/kubeedge/cloud/pkg/devicecontroller"
 	"github.com/kubeedge/kubeedge/cloud/pkg/dynamiccontroller"
 	"github.com/kubeedge/kubeedge/cloud/pkg/edgecontroller"
+	"github.com/kubeedge/kubeedge/cloud/pkg/iptablesmgr"
 	"github.com/kubeedge/kubeedge/cloud/pkg/router"
 	"github.com/kubeedge/kubeedge/cloud/pkg/synccontroller"
 	"github.com/kubeedge/kubeedge/common/constants"
@@ -91,16 +91,17 @@ kubernetes controller which manages devices so that the device metadata/status d
 
 			registerModules(config)
 
+			ctx := beehiveContext.GetContext()
 			if config.Modules.IptablesManager == nil || config.Modules.IptablesManager.Enable && config.Modules.IptablesManager.Mode == v1alpha1.InternalMode {
 				// By default, IptablesManager manages tunnel port related iptables rules
 				// The internal mode will share the host network, forward to the stream port.
 				streamPort := int(config.Modules.CloudStream.StreamPort)
-				go iptables.NewIptablesManager(streamPort).Run()
+				go iptablesmgr.NewIptablesManager(config.KubeAPIConfig, streamPort).Run(ctx)
 			}
 
 			// Start all modules
 			core.StartModules()
-			gis.Start(beehiveContext.Done())
+			gis.Start(ctx.Done())
 			core.GracefulShutdown()
 		},
 	}

cloud/cmd/iptablesmanager/app/server.go
@@ -17,6 +17,7 @@ limitations under the License.
 package app
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
@@ -28,11 +29,8 @@ import (
 	"k8s.io/component-base/term"
 	"k8s.io/klog/v2"
 
-	beehiveContext "github.com/kubeedge/beehive/pkg/core/context"
 	"github.com/kubeedge/kubeedge/cloud/cmd/iptablesmanager/app/options"
-	"github.com/kubeedge/kubeedge/cloud/pkg/cloudstream/iptables"
-	"github.com/kubeedge/kubeedge/cloud/pkg/common/client"
-	"github.com/kubeedge/kubeedge/cloud/pkg/common/informers"
+	"github.com/kubeedge/kubeedge/cloud/pkg/iptablesmgr"
 	"github.com/kubeedge/kubeedge/common/constants"
 	"github.com/kubeedge/kubeedge/pkg/apis/componentconfig/cloudcore/v1alpha1"
 	"github.com/kubeedge/kubeedge/pkg/util/flag"
@@ -48,7 +46,10 @@ func NewIptablesManagerCommand() *cobra.Command {
 			verflag.PrintAndExitIfRequested()
 			flag.PrintFlags(cmd.Flags())
 
-			// init kubeedge client
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			// The external mode will share the host network, forward to the inner stream port of the cloudcore.
 			kubeAPIConfig := &v1alpha1.KubeAPIConfig{
 				Master:      "",
 				ContentType: constants.DefaultKubeContentType,
@@ -56,14 +57,7 @@ func NewIptablesManagerCommand() *cobra.Command {
 				Burst:       constants.DefaultKubeBurst,
 				KubeConfig:  opts.KubeConfig,
 			}
-			client.InitKubeEdgeClient(kubeAPIConfig)
-
-			gis := informers.GetInformersManager()
-			ctx := beehiveContext.GetContext()
-
-			// The external mode will share the host network, forward to the inner stream port of the cloudcore.
-			go iptables.NewIptablesManager(opts.ForwardPort).Run()
-			gis.Start(ctx.Done())
+			go iptablesmgr.NewIptablesManager(kubeAPIConfig, opts.ForwardPort).Run(ctx)
 
 			c := make(chan os.Signal, 1)
 			signal.Notify(c, syscall.SIGINT, syscall.SIGHUP, syscall.SIGTERM,

gfk-kube/kubeedge-lite

Former-commit-id: a6016ab802747a4f2710f7d4558b7b3adcbc34d3

cloud/cmd/cloudcore/app/server.go
@@ -91,16 +91,17 @@ kubernetes controller which manages devices so that the device metadata/status d
 
 			registerModules(config)
 
+			ctx := beehiveContext.GetContext()
 			if config.Modules.IptablesManager == nil || config.Modules.IptablesManager.Enable && config.Modules.IptablesManager.Mode == v1alpha1.InternalMode {
 				// By default, IptablesManager manages tunnel port related iptables rules
 				// The internal mode will share the host network, forward to the stream port.
 				streamPort := int(config.Modules.CloudStream.StreamPort)
-				go iptables.NewIptablesManager(streamPort).Run()
+				go iptables.NewIptablesManager(config.KubeAPIConfig, streamPort).Run(ctx)
 			}
 
 			// Start all modules
 			core.StartModules()
-			gis.Start(beehiveContext.Done())
+			gis.Start(ctx.Done())
 			core.GracefulShutdown()
 		},
 	}

cloud/cmd/iptablesmanager/app/server.go
@@ -17,6 +17,7 @@ limitations under the License.
 package app
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
@@ -28,11 +29,8 @@ import (
 	"k8s.io/component-base/term"
 	"k8s.io/klog/v2"
 
-	beehiveContext "github.com/kubeedge/beehive/pkg/core/context"
 	"github.com/kubeedge/kubeedge/cloud/cmd/iptablesmanager/app/options"
 	"github.com/kubeedge/kubeedge/cloud/pkg/cloudstream/iptables"
-	"github.com/kubeedge/kubeedge/cloud/pkg/common/client"
-	"github.com/kubeedge/kubeedge/cloud/pkg/common/informers"
 	"github.com/kubeedge/kubeedge/common/constants"
 	"github.com/kubeedge/kubeedge/pkg/apis/componentconfig/cloudcore/v1alpha1"
 	"github.com/kubeedge/kubeedge/pkg/util/flag"
@@ -48,7 +46,10 @@ func NewIptablesManagerCommand() *cobra.Command {
 			verflag.PrintAndExitIfRequested()
 			flag.PrintFlags(cmd.Flags())
 
-			// init kubeedge client
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			// The external mode will share the host network, forward to the inner stream port of the cloudcore.
 			kubeAPIConfig := &v1alpha1.KubeAPIConfig{
 				Master:      "",
 				ContentType: constants.DefaultKubeContentType,
@@ -56,14 +57,7 @@ func NewIptablesManagerCommand() *cobra.Command {
 				Burst:       constants.DefaultKubeBurst,
 				KubeConfig:  opts.KubeConfig,
 			}
-			client.InitKubeEdgeClient(kubeAPIConfig)
-
-			gis := informers.GetInformersManager()
-			ctx := beehiveContext.GetContext()
-
-			// The external mode will share the host network, forward to the inner stream port of the cloudcore.
-			go iptables.NewIptablesManager(opts.ForwardPort).Run()
-			gis.Start(ctx.Done())
+			go iptables.NewIptablesManager(kubeAPIConfig, opts.ForwardPort).Run(ctx)
 
 			c := make(chan os.Signal, 1)
 			signal.Notify(c, syscall.SIGINT, syscall.SIGHUP, syscall.SIGTERM,