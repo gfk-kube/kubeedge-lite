58ad037d75733fb9be138fb572245fc9d55f5f89
sam#gemmi-win10 <sam@china.gz>
2023-04-13 20:53:30 +0800

    sq: up build_with_container.sh -- -v /_ext/ke_go:/go \
    notes dbg cmd
    
    UID=0
    
    DOCKER_GID=0 #root
    
    dbg
    
    up

--- build_with_container.sh
+++ build_with_container.sh
@@ -16,20 +16,37 @@
 #limitations under the License.
 ###
 
-set -o errexit
-set -o nounset
-set -o pipefail
+# set -o errexit
+# set -o nounset
+# set -o pipefail
 
-KUBEEDGE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd -P)"
+KUBEEDGE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/" && pwd -P)" #/../..
 MOUNTPATH="${MOUNTPATH:-/kubeedge}"
 KUBEEDGE_BUILD_IMAGE=${KUBEEDGE_BUILD_IMAGE:-"kubeedge/build-tools:1.17.13-ke1"}
 DOCKER_GID="${DOCKER_GID:-$(grep '^docker:' /etc/group | cut -f3 -d:)}"
 CONTAINER_RUN_OPTIONS="${CONTAINER_RUN_OPTIONS:--it}"
+# 
+DOCKER_GID=0 #root
+UID=0 #默认的: mkdir /go 无权限;
 
 echo "start building inside container"
+echo docker run --rm ${CONTAINER_RUN_OPTIONS} -u "${UID}:${DOCKER_GID}" \
+    --init \
+    --sig-proxy=true \
+    -e XDG_CACHE_HOME=/tmp/.cache \
+    -v /_ext/ke_go:/go \
+    -v ${KUBEEDGE_ROOT}:${MOUNTPATH} \
+    -w ${MOUNTPATH} ${KUBEEDGE_BUILD_IMAGE} "$@"
+# docker run --rm -it -u 0:0 --init --sig-proxy=true -e XDG_CACHE_HOME=/tmp/.cache -v /_ext/working/_ct/fk-kubeedge-bfg-b1:/kubeedge -w /kubeedge kubeedge/build-tools:1.17.13-ke1 ./build.sh
+# # headless @ mac23-199 in .../_ct/fk-kubeedge-bfg-b1 |21:23:42  |fix-offline-pods ?:6 ✗| 
+# $ git pull; docker run --rm -it -u 0:0 --init --sig-proxy=true -e XDG_CACHE_HOME=/tmp/.cache -v /_ext/ke_go:/go -v /_ext/working/_ct/fk-kubeedge-bfg-b1:/kubeedge -w /kubeedge kubeedge/build-tools:1.17.13-ke1 bash
+
+# exit 0
+
 docker run --rm ${CONTAINER_RUN_OPTIONS} -u "${UID}:${DOCKER_GID}" \
     --init \
     --sig-proxy=true \
     -e XDG_CACHE_HOME=/tmp/.cache \
+    -v /_ext/ke_go:/go \
     -v ${KUBEEDGE_ROOT}:${MOUNTPATH} \
     -w ${MOUNTPATH} ${KUBEEDGE_BUILD_IMAGE} "$@"
